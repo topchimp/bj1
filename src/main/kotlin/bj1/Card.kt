a5629c32cc73b16727d43fe4c17e2c5952309361
sdavies <sdavies@thoughtworks.com>
2018-11-17 16:46:28 +0000

    Added Points and a prettyprint for cards

--- src/main/kotlin/bj1/Card.kt
+++ src/main/kotlin/bj1/Card.kt
@@ -1,3 +1,32 @@
 package bj1
 
-class Card (val value:Int, val suit:Int)
+class Card(val value: Int, val suit: Int) {
+    val suitName: String
+        get() = when (suit) {
+            1 -> "Spades"
+            2 -> "Hearts"
+            3 -> "Clubs"
+            4 -> "Diamonds"
+            else -> throw IllegalStateException()
+        }
+
+    val valueName: String
+        get() = when (value) {
+            1 -> "Ace"
+            in 2..10 -> value.toString()
+            11->"Jack"
+            12->"Queen"
+            13->"King"
+
+            else -> throw IllegalStateException()
+        }
+
+    val name:String get() = "$valueName of $suitName"
+
+    val points:Int get() = when(value){
+        in 1..9 -> value
+        in 10..13 -> 10
+        else -> throw IllegalStateException()
+    }
+
+}
--- src/test/kotlin/bj1/CardTest.kt
+++ src/test/kotlin/bj1/CardTest.kt
@@ -5,20 +5,97 @@ import kotlin.test.assertEquals
 
 class CardTest {
     @Test
-    fun t1(){
-        val c1 = Card(1,1)
-        val c2 = Card(13,4)
-        assertEquals(1,c1.value)
-        assertEquals(1,c1.suit)
+    fun t1() {
+        val c1 = Card(1, 1)
+        //Named params
+        val c2 = Card(value = 13, suit = 4)
+        assertEquals(1, c1.value)
+        assertEquals(1, c1.suit)
+        assertEquals("Spades", c1.suitName)
+        assertEquals("Ace", c1.valueName)
+        assertEquals("Ace of Spades", c1.name)
+        assertEquals(1, c1.points)
 
-        assertEquals(13,c2.value)
-        assertEquals(4,c2.suit)
+
+        assertEquals(13, c2.value)
+        assertEquals(4, c2.suit)
+        assertEquals("Diamonds", c2.suitName)
+        assertEquals("King", c2.valueName)
+        assertEquals("King of Diamonds", c2.name)
+        assertEquals(10, c2.points)
+
+
+
 
     }
+
+    @Test
+    fun testSuit4IsDiamonds() {
+        val c1 = Card(1, 4)
+        assertEquals("Diamonds", c1.suitName)
+    }
+
+    @Test
+    fun testSuit3IsClubs() {
+        val c1 = Card(1, 3)
+        assertEquals("Clubs", c1.suitName)
+    }
+
+    @Test
+    fun testSuit2IsHearts() {
+        val c1 = Card(1, 2)
+        assertEquals("Hearts", c1.suitName)
+    }
+
+    @Test
+    fun testSuit1IsSpades() {
+        val c1 = Card(1, 1)
+        assertEquals("Spades", c1.suitName)
+    }
+
+    @Test
+    fun testACardWithValue13IsAKing() {
+        val c1 = Card(13, 1)
+        assertEquals("King", c1.valueName)
+    }
+
+    @Test
+    fun testACardWithValue12IsAQueen() {
+        val c1 = Card(12, 1)
+        assertEquals("Queen", c1.valueName)
+    }
+
+
+    @Test
+    fun testACardWithValue11IsAJack() {
+        val c1 = Card(11, 1)
+        assertEquals("Jack", c1.valueName)
+    }
+
+    @Test
+    fun testACardWithValue1IsAnAce() {
+        val c1 = Card(1, 1)
+        assertEquals("Ace", c1.valueName)
+    }
+
+    @Test
+    fun testPrettyPrintOfAceOfSpades() {
+        val c1 = Card(1, 1)
+        assertEquals("Ace of Spades", c1.name)
+    }
+
+
+    @Test
+    fun testAnAceIsWorthOnePoint() {
+        val c1 = Card(1, 1)
+        assertEquals(1, c1.points)
+    }
+
+
     @Test
     fun testCanCreateACardWithSuitAndValue() {
-        val c1 = Card(1,1)
-        assertEquals(1,c1.value)
+        val c1 = Card(1, 1)
+        assertEquals(1, c1.value)
 
     }
 
